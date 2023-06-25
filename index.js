1a770260766b32144d4d3eb5727dfc3465021f68
Jason Silvis <JSilvis030592@yahoo.com>
2023-06-25 01:15:03 -0400

    Modifying the elements I want to target in the future.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-$("div.upper").text("Hello");
+
+var guestName = prompt("What is your name?");
+var upperInnerContent = "Hello, " + guestName;
+
+
+$(".upper-inside").text(upperInnerContent);
+$(".middle-inside").text("This will be an image later");
+$(".lower-inside").text("Lower")
