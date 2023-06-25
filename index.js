451ff182cacb6fbd4a03bb2a35b0a01ebfa916c8
Jason Silvis <JSilvis030592@yahoo.com>
2023-06-25 13:03:23 -0400

    Worked on the functionality of the lower box

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 var upperWrkText = "hello";
 var wrkImg = "./assets/images/neil-rosenstech-KG-9TUrAFsg-unsplash.jpg";
+var lowerWrkText = "Action List";
+
+
 
 var randomNum = Math.floor(Math.random() * 10);
 
 if (randomNum < 5) {
-    upperWrkText = "Welcome Adventurer!"
+    upperWrkText = "Welcome Adventurer!";
+    lowerWrkText = "What do you choose to do?";
+   
 }
 else {
-    upperWrkText = "Greetings Explorer!"
+    upperWrkText = "Greetings Explorer!";
+    lowerWrkText = "We must take action!";
+    
 }
 
 
@@ -21,4 +28,4 @@ var middleInnerContent = wrkImg;
 
 $(".upper-inside").text(upperInnerContent);
 $(".image").attr("src",wrkImg);
-$(".lower-inside").text("Lower")
+$(".lower-inside").text(lowerWrkText);
