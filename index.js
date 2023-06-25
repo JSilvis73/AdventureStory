790447157df2e2d42e93b3d3f0e195723d6297f1
Jason Silvis <JSilvis030592@yahoo.com>
2023-06-25 18:29:23 -0400

    Testing out manipulating pictures

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var upperWrkText = "hello";
-var wrkImg = "./assets/images/neil-rosenstech-KG-9TUrAFsg-unsplash.jpg";
-var lowerWrkText = "Action List";
+var wrkImg = "";
+var lowerWrkText = ["1: Stand ground!", 
+" 2: Run!"];
 
 
 
@@ -8,12 +9,16 @@ var randomNum = Math.floor(Math.random() * 10);
 
 if (randomNum < 5) {
     upperWrkText = "Welcome Adventurer!";
-    lowerWrkText = "What do you choose to do?";
+    wrkImg = "./assets/images/neil-rosenstech-KG-9TUrAFsg-unsplash.jpg"
+    lowerWrkText = ["1: Stand ground!", 
+" 2: Run!"];
    
 }
 else {
     upperWrkText = "Greetings Explorer!";
-    lowerWrkText = "We must take action!";
+    wrkImg = "./assets/images/angel-luciano--hWwL0n3_As-unsplash.jpg"
+    lowerWrkText = ["1: FIGHT!", 
+" 2: Cower!?"];
     
 }
 
@@ -29,3 +34,6 @@ var middleInnerContent = wrkImg;
 $(".upper-inside").text(upperInnerContent);
 $(".image").attr("src",wrkImg);
 $(".lower-inside").text(lowerWrkText);
+
+
+// Functions
